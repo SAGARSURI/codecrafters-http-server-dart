beca36fcc24a01a5c9a72a86539ee58c74098ddb
Sagar Suri <sagarsuri56@gmail.com>
2024-09-01 10:23:02 +0530

    codecrafters submit [skip ci]

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 const http200 = 'HTTP/1.1 200 OK\r\n\r\n';
-const http400 = 'HTTP/1.1 404 Not Found\r\n\r\n';
+const http404 = 'HTTP/1.1 404 Not Found\r\n\r\n';
 void main() async {
   var serverSocket = await ServerSocket.bind('0.0.0.0', 4221);
 
@@ -12,12 +12,64 @@ void main() async {
       final requestLine = request = request.split('\r\n')[0];
       final path = requestLine.split(' ')[1];
 
-      if (path != '/') {
-        clientSocket.write(http400);
-      } else {
+      if (path == '/') {
         clientSocket.write(http200);
+        return;
+      }
+
+      final pathSegments = path.split('/');
+      if (pathSegments.length > 2 && pathSegments[1] == 'echo') {
+        clientSocket.write(
+          buildResponse(
+            ResponseType.ok,
+            body: pathSegments.last,
+          ),
+        );
+      } else {
+        clientSocket.write(http404);
       }
       clientSocket.close();
     });
   }
+}
+
+String buildResponse(ResponseType responseType, {String? body}) {
+  final contentLength = body?.length;
+  final contentType = 'text/plain';
+  return responseBuilder(
+    responseType: responseType,
+    contentType: contentType,
+    contentLength: contentLength.toString(),
+    body: body,
+  );
+}
+
+String responseBuilder({
+  required ResponseType responseType,
+  String? contentType,
+  String? contentLength,
+  String? body,
+}) {
+  String statusLine =
+      'HTTP/1.1 ${responseType.code} ${responseType.message}\r\n';
+  if (contentType != null) {
+    statusLine += 'Content-Type: $contentType\r\n';
+  }
+  if (contentLength != null) {
+    statusLine += 'Content-Length: $contentLength\r\n';
+  }
+  statusLine += '\r\n';
+  if (body != null) {
+    statusLine += body;
+  }
+  return statusLine;
+}
+
+enum ResponseType {
+  ok(200, 'OK'),
+  notFound(404, 'Not Found');
+
+  const ResponseType(this.code, this.message);
+  final int code;
+  final String message;
 }
